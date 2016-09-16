261371ba2d238fb71e4c82826209a19d441fa5a4
ynw <ynw_soft@yahoo.co.jp>
2016-09-17 08:17:38 +0900

    gcovr runnner add & model fix for coverage (no exception throw)

--- Model/CardData.cpp
+++ Model/CardData.cpp
@@ -59,42 +59,39 @@ CardData::CardData(std::vector<std::string> raw_data)
 {
 	convert_error = false;
 	int card_count = 0;
-	try {
-		for (std::string line : raw_data) {
-			if (line[0] == '#')continue;	// skip comment line
+	for (std::string line : raw_data) {
+		if (line[0] == '#')continue;	// skip comment line
 
-			std::vector<std::string> token = StringUtility::SplitString(line);
-			if (token.size() != Column::COLUMN_NUM) {
-				throw "line column num error";
-			}
-			if (atoi(token[Column::ID].c_str()) != card_count) {
-				throw "ID not mutch error";
-			}
-
-			CardDataSub* subdata = new CardDataSub();
-			subdata->filename = token[Column::FILE_NAME];
-			subdata->name = token[Column::NAME];
-			subdata->idol_name = token[Column::SIMPLE_NAME];
-			subdata->one_word = token[Column::ONE_WORD];
-			subdata->card_type = atoCardType(token[Column::CARD_TYPE]);
-			subdata->point = atoi(token[Column::POINT].c_str());
-			subdata->skill_type = atoSkilltype(token[Column::SKILL]);
-			subdata->happning_cost = atoi(token[Column::COST].c_str());
-			subdata->happning_burst_cost = atoi(token[Column::BURST_COST].c_str());
-			subdata->join_event_flag = atoEventFlag(token[Column::JOIN_EVENT]);
-			subdata->open_event_flag = atoEventFlag(token[Column::OPEN_EVENT]);
-			subdata->card_col = atoCardColor(token[Column::COLOR]);
-			subdata->idol_type = atoIdolType(token[Column::IDOL_TYPE]);
-			subdata->character[0] = token[Column::CHAR1];
-			subdata->character[1] = token[Column::CHAR2];
-			subdata->character[2] = token[Column::CHAR3];
-
-			cards.push_back(subdata);
-			card_count++;
+		std::vector<std::string> token = StringUtility::SplitString(line);
+		if (token.size() != Column::COLUMN_NUM) {
+			convert_error = true;
+			break;
 		}
-	}
-	catch (const char*) {
-		convert_error = true;
+		if (atoi(token[Column::ID].c_str()) != card_count) {
+			convert_error = true;
+			break;
+		}
+
+		CardDataSub* subdata = new CardDataSub();
+		subdata->filename = token[Column::FILE_NAME];
+		subdata->name = token[Column::NAME];
+		subdata->idol_name = token[Column::SIMPLE_NAME];
+		subdata->one_word = token[Column::ONE_WORD];
+		subdata->card_type = atoCardType(token[Column::CARD_TYPE]);
+		subdata->point = atoi(token[Column::POINT].c_str());
+		subdata->skill_type = atoSkilltype(token[Column::SKILL]);
+		subdata->happning_cost = atoi(token[Column::COST].c_str());
+		subdata->happning_burst_cost = atoi(token[Column::BURST_COST].c_str());
+		subdata->join_event_flag = atoEventFlag(token[Column::JOIN_EVENT]);
+		subdata->open_event_flag = atoEventFlag(token[Column::OPEN_EVENT]);
+		subdata->card_col = atoCardColor(token[Column::COLOR]);
+		subdata->idol_type = atoIdolType(token[Column::IDOL_TYPE]);
+		subdata->character[0] = token[Column::CHAR1];
+		subdata->character[1] = token[Column::CHAR2];
+		subdata->character[2] = token[Column::CHAR3];
+
+		cards.push_back(subdata);
+		card_count++;
 	}
 
 	if (cards.size() == 0) {
---
+++ gcovr.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+export PRJ=IDOL_VERSUS
+export RESULT=ModelCoverage.xml
+
+# export GTEST=~/googletest-master # set at jenkins
+export GTLIB=${GTEST}/build/gtest
+export GMLIB=${GTEST}/build/googlemock
+
+mkdir build
+cd build
+
+echo "################################################################################"
+echo "## BUILD MODEL"
+echo "################################################################################"
+g++ -std=c++11 -O2 -fno-exceptions -W -Wall -c ../Model/*.cpp -DLINUX -fprofile-arcs -ftest-coverage
+ar r libstatic.a *.o
+
+echo "################################################################################"
+echo "## BUILD TEST"
+echo "################################################################################"
+g++ -std=c++11 -O2 -fno-exceptions -W -Wall ../ModelTest/*.cpp -DLINUX -fprofile-arcs -ftest-coverage \
+    -I../Model libstatic.a \
+    -I${GTEST}/googletest/include ${GTLIB}/libgtest.a ${GTLIB}/libgtest_main.a \
+    -I${GTEST}/googlemock/include ${GMLIB}/libgmock.a ${GMLIB}/libgmock_main.a \
+    -lpthread -o test
+
+echo "################################################################################"
+echo "## RUN TEST"
+echo "################################################################################"
+./test
+
+echo "################################################################################"
+echo "## COVERAGE"
+echo "################################################################################"
+gcovr -r ~/jenkins/${PRJ}/Model --xml --output=${RESULT} .
+vim -c %s/filename=\"/\\0Model\\//g -c x ${RESULT}
+
+cp ${RESULT} /home/samba/jenkins/
